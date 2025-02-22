c279a27c75bf36612381f9e45d3420d59b605b48
Sharon Shechter <sharonshechter1@gmail.com>
2025-02-22 20:55:25 +0200

    Set up project directories

---
+++ backend/models/userModel.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime
+import datetime
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    role = Column(String, default="student")
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone_number = Column(String, nullable=True)
+    address = Column(String, nullable=True)
+    class_name = Column(String, nullable=True)
+    lesson = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
